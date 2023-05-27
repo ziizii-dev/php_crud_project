7a335bb70a2795df8f3863bd709ede81a35fe23b
ziizii-dev <ziizii.dev@gmail.com>
2023-05-27 23:33:30 +0630

    server_data_update

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
           $host = 'localhost';
-          $dbname = 'php_course';
+          $dbname = 'ziizii';
           $user = 'root';
-          $password = '123456'; 
+          $password = 'Hello*111#'; 
              try {
               $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
               $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
