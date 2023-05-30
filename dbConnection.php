b962530504054dd2f31cbfb446782b9b7de737a9
ziizii-dev <ziizii.dev@gmail.com>
2023-05-30 09:55:05 +0630

    after spread php and html files

--- dbConnection.php
+++ dbConnection.php
@@ -1,12 +1,12 @@
 <?php
-          $host = 'localhost';
-          $dbname = 'php_course';
-          $user = 'root';
-          $password = 123456; 
-        // $host = 'localhost';
-        // $dbname = 'ziizii';
-        // $user = 'root';
-        // $password = "Hello*111#";
+        //   $host = 'localhost';
+        //   $dbname = 'php_course';
+        //   $user = 'root';
+        //   $password = 123456; 
+        $host = 'localhost';
+        $dbname = 'ziizii';
+        $user = 'root';
+        $password = "Hello*111#";
              try {
               $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
               $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
